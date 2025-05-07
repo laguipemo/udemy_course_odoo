8fc3d649dba5224a172332df1bdda4e39973197c
Lázaro Guillermo Pérez Montoto <laguipemo@gmail.com>
2025-05-07 07:50:34 +0200

    Implemented a qweb-pdf report and

--- models/property.py
+++ models/property.py
@@ -45,14 +45,14 @@ class Property(models.Model):
     postcode = fields.Char(string="Postcode")
     date_availability = fields.Date(string="Available From")
     expected_price = fields.Float(string="Expected Price")
-    best_offer = fields.Float(streing="Best Offer", compute="_compute_best_offer")
+    best_offer = fields.Float(string="Best Offer", compute="_compute_best_offer")
     selling_price = fields.Float(string="Selling Price")
     bedrooms = fields.Integer(string="Bedrooms")
-    living_area = fields.Integer(strling="Living Area(sqm)")
-    facades = fields.Integer(strling="Facades")
-    garage = fields.Boolean(strling="Garage", default=False)
-    garden = fields.Boolean(strling="Garden", default=False)
-    garden_area = fields.Integer(strling="Garden Area(sqm)")
+    living_area = fields.Integer(string="Living Area(sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage", default=False)
+    garden = fields.Boolean(string="Garden", default=False)
+    garden_area = fields.Integer(string="Garden Area(sqm)")
     garden_orientation = fields.Selection(
         string="Garden Orientation",
         selection=[
